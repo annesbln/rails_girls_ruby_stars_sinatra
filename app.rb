df4e1483f107b051d500716db3a0665a5f799541
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2018-11-30 23:51:15 +0100

    Use sinatra dsl for views and add a route to demonstrate it

--- app.rb
+++ app.rb
@@ -12,3 +12,8 @@ end
 get "/use_erb/:name" do
   ERB.new("<html><body><h1>Hello <%= params[:name] %>!</h1></body></html>").result(binding)
 end
+
+get "/use_sinatra_supported_erb/:name" do
+  # erb "<html><body><h1>Hello <%= name %>!</h1></body></html>", { :locals => { :name => params[:name] } }
+  erb "<html><body><h1>Hello <%= name %>!</h1></body></html>", { :locals => params }
+end
