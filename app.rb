189c14d3392820d4eea279e5fa8c6c4bc19b0278
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2018-11-30 23:49:59 +0100

    Add say_hello to explain route params

--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ require "sinatra"
 get "/" do
   "<html><body><h1>Hello World!</h1></body></html>"
 end
+
+get "/say_hello/:name" do
+  "<html><body><h1>Hello #{params[:name]}!</h1></body></html>"
+end
