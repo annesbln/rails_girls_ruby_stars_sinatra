5b021aab6f00ca06dcc1f69cda675d883019f710
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2018-11-30 23:50:32 +0100

    Use ERB standard library and add a route to demonstrate it

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "erb"
 
 get "/" do
   "<html><body><h1>Hello World!</h1></body></html>"
@@ -6,4 +7,8 @@ end
 
 get "/say_hello/:name" do
   "<html><body><h1>Hello #{params[:name]}!</h1></body></html>"
+end
+
+get "/use_erb/:name" do
+  ERB.new("<html><body><h1>Hello <%= params[:name] %>!</h1></body></html>").result(binding)
 end
